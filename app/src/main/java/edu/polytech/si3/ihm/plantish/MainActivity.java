97cc7bb3716bfacfff53391e1d70af22431d54d5
Marine Demonchaux <marine.demonchaux@etu.unice.fr>
2021-04-14 15:52:12 +0200

    Add User class

--- app/src/main/java/edu/polytech/si3/ihm/plantish/MainActivity.java
+++ app/src/main/java/edu/polytech/si3/ihm/plantish/MainActivity.java
@@ -2,7 +2,10 @@ package edu.polytech.si3.ihm.plantish;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button buttonAdd = (Button) findViewById(R.id.addButton);
+        addListenerToAddPlant(buttonAdd);
+    }
+
+    private void addListenerToAddPlant(Button button){
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AddPlantActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/edu/polytech/si3/ihm/plantish/User.java
@@ -0,0 +1,5 @@
+package edu.polytech.si3.ihm.plantish;
+
+public class User {
+    private String name;
+}
