3fe9b58eb8a761761f4df3ff039e9f75dd657970
Marine Demonchaux <marine.demonchaux@etu.unice.fr>
2021-05-03 14:41:46 +0200

    Refractor PlantActivity and add Scroll on View

--- app/src/main/java/edu/polytech/si3/ihm/plantish/AddPlantActivity.java
+++ app/src/main/java/edu/polytech/si3/ihm/plantish/AddPlantActivity.java
@@ -81,24 +81,9 @@ import edu.polytech.si3.ihm.plantish.user.User;
  * @author Marine Demonchaux
  */
 
-public class AddPlantActivity extends AppCompatActivity {
+public class AddPlantActivity extends PlantActivity {
 
-    EditText date;
-    EditText description;
-    Spinner spinnerType;
-    Spinner spinnerFamily;
-    Bitmap bitmap;
-    User user;
 
-    DatePickerDialog datePickerDialog;
-    ImageButton imageButton;
-    private static final int CAMERA_REQUEST = 1888;
-    private ImageView imageView;
-    private static final int MY_CAMERA_PERMISSION_CODE = 100;
-    private static final int GALLERY_REQUEST = 2;
-    private GeoPoint position;
-    MapView map;
-    protected static final int DEFAULT_INACTIVITY_DELAY_IN_MILLISECS = 1 ;
 
 
     @Override
@@ -187,85 +172,7 @@ public class AddPlantActivity extends AppCompatActivity {
 
     }
 
-    private int getResource(){
-        String typeSelected = this.spinnerType.getSelectedItem().toString();
-        if(englishToFrench(Plant.BUSH).equals(typeSelected))
-            return R.array.familybush;
-        else if(englishToFrench(Plant.TREE).equals(typeSelected))
-            return R.array.familytree;
-        else return R.array.familyflower;
 
-    }
-
-    private int getPositionStringInArrayRes(int id, String string){
-        List<String> list = Arrays.asList(getResources().getStringArray(id));
-        string = englishToFrench(string);
-        for(int i = 0; i < list.size(); i++){
-            if(list.get(i).equalsIgnoreCase(string)) return i;
-        }
-        return 0;
-    }
-
-    private String englishToFrench(String s){
-        if(s.equals("FLOWER")) return "Fleur";
-        if(s.equals("BUSH")) return "Buisson";
-        if(s.equals("TREE")) return "Arbre";
-        else return s;
-    }
-
-    private void setLocationOnEditText(){
-        Geocoder geocoder;
-        List<Address> addresses = null;
-        geocoder = new Geocoder(this, Locale.getDefault());
-
-        try {
-            addresses = geocoder.getFromLocation(position.getLatitude(), position.getLongitude(), 1); // Here 1 represent max location result to returned, by documents it recommended 1 to 5
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        String address = addresses.get(0).getAddressLine(0); // If any additional address line present than only, check with max available address lines by getMaxAddressLineIndex()
-
-        EditText editAddress = (EditText) findViewById(R.id.editTextAdress);
-        editAddress.setText(address);
-
-        editAddress.setOnKeyListener(new View.OnKeyListener() {
-            @Override
-            public boolean onKey(View v, int keyCode, KeyEvent event) {
-                String string = editAddress.getText().toString();
-                LatLng latlngLocation = AddPlantActivity.this.getLocationFromAddress(AddPlantActivity.this, string);
-                position.setLatitude(latlngLocation.latitude);
-                position.setLongitude(latlngLocation.longitude);
-                initializeMap(position);
-                return true;
-
-            }
-        });
-    }
-    public LatLng getLocationFromAddress(Context context,String strAddress) {
-
-        Geocoder coder = new Geocoder(context);
-        List<Address> address;
-        LatLng p1 = null;
-
-        try {
-            address = coder.getFromLocationName(strAddress, 5);
-            if (address == null) {
-                return null;
-            }
-            Address location = address.get(0);
-            location.getLatitude();
-            location.getLongitude();
-
-            p1 = new LatLng(location.getLatitude(), location.getLongitude() );
-
-        } catch (Exception ex) {
-
-            ex.printStackTrace();
-        }
-
-        return p1;
-    }
 
     private boolean addPlant(Button addButton){
 
@@ -307,161 +214,14 @@ public class AddPlantActivity extends AppCompatActivity {
 
     }
 
-    private void setCalendar(){
-        date.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // calender class's instance and get current date , month and year from calender
-                final Calendar c = Calendar.getInstance();
-                int mYear = c.get(Calendar.YEAR); // current year
-                int mMonth = c.get(Calendar.MONTH); // current month
-                int mDay = c.get(Calendar.DAY_OF_MONTH); // current day
-                // date picker dialog
-                datePickerDialog = new DatePickerDialog(AddPlantActivity.this,
-                        new DatePickerDialog.OnDateSetListener() {
-
-                            @Override
-                            public void onDateSet(DatePicker view, int year,
-                                                  int monthOfYear, int dayOfMonth) {
-                                // set day of month , month and year value in the edit text
-                                date.setText(String.format("%d/%d/%d", dayOfMonth, monthOfYear + 1, year));
-                            }
-                        }, mYear, mMonth, mDay);
-                datePickerDialog.show();
-            }
-        });
-    }
-
-
-    private void galleryButton(){
-        imageButton = (ImageButton) findViewById(R.id.galleryBtn);
-        imageButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
-                photoPickerIntent.setType("image/*");
-                startActivityForResult(photoPickerIntent, GALLERY_REQUEST);
-            }});
-    }
 
 
 
-    private void cameraButton(){
-        imageButton = (ImageButton) findViewById(R.id.cameraBtn);
-        imageButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-                    requestPermissions(new String[]{Manifest.permission.CAMERA}, MY_CAMERA_PERMISSION_CODE);
-                } else {
-                    Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                    startActivityForResult(cameraIntent, CAMERA_REQUEST);
-                }
-            }});
-    }
-
-    private void initializeMap(GeoPoint position){
-        map = findViewById(R.id.mapObject1);
-        map.getOverlays().clear();
-        map.setTileSource(TileSourceFactory.MAPNIK); //Render
-        map.setMultiTouchControls(true);
-
-        GeoPoint startPoint = new GeoPoint(position.getLatitude(), position.getLongitude()); //Données de Marine ici
-        IMapController controller = map.getController();
-        controller.setZoom(19.2); //Valeur par défault du zoom
-        controller.setCenter(startPoint); //Centrer la carte sur ce point
-
-        setOverlayItem(map, startPoint);
-        Log.d("DRAG", startPoint+"");
-
-        map.setMapListener(new DelayedMapListener(new MapListener() {
-            @Override
-            public boolean onScroll(ScrollEvent event) {
-                position.setLatitude(map.getMapCenter().getLatitude());
-                position.setLongitude(map.getMapCenter().getLongitude());
-                map.getOverlays().clear();
-                setOverlayItem(map, new GeoPoint(position.getLatitude(), position.getLongitude()));
-                setLocationOnEditText();
-                return false;
-            }
-
-            @Override
-            public boolean onZoom(ZoomEvent event) {
-                position.setLatitude(map.getMapCenter().getLatitude());
-                position.setLongitude(map.getMapCenter().getLongitude());
-                map.getOverlays().clear();
-                setOverlayItem(map, new GeoPoint(position.getLatitude(), position.getLongitude()));
-                setLocationOnEditText();
-                return false;
-            }
-        }, DEFAULT_INACTIVITY_DELAY_IN_MILLISECS));
 
 
-    }
 
-    private void setOverlayItem(MapView map, GeoPoint startPoint){
-        List<OverlayItem> marqueurs = new ArrayList<>();
-        OverlayItem home = new OverlayItem("Votre Plante", "", startPoint);
-        Bitmap d =  BitmapFactory.decodeResource(MainActivity.context.getResources(), R.mipmap.grass); //Changer l'image du marqueur, ici un qui existe déjà
-        BitmapDrawable dd = new BitmapDrawable(getResources(), Bitmap.createScaledBitmap(d, 80, 80, true));
-        home.setMarker(dd);
-        marqueurs.add(home);
 
-        ItemizedOverlayWithFocus<OverlayItem> itemizedOverlayWithFocus = new ItemizedOverlayWithFocus<>(getApplicationContext(), marqueurs, new ItemizedIconOverlay.OnItemGestureListener<OverlayItem>() {
-            @Override
-            public boolean onItemSingleTapUp(int index, OverlayItem item) {
-                return true; // Pour qu'il réagisse quand j'ai cliqué dessus
-            }
 
-            @Override
-            public boolean onItemLongPress(int index, OverlayItem item) {
-                return false;
-            }
-        });
-        itemizedOverlayWithFocus.setFocusItemsOnTap(true);
-        map.getOverlays().add(itemizedOverlayWithFocus);
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults)
-    {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (requestCode == MY_CAMERA_PERMISSION_CODE)
-        {
-            if (grantResults[0] == PackageManager.PERMISSION_GRANTED)
-            {
-                Toast.makeText(this, "camera permission granted", Toast.LENGTH_LONG).show();
-                Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(cameraIntent, CAMERA_REQUEST);
-            }
-            else
-            {
-                Toast.makeText(this, "camera permission denied", Toast.LENGTH_LONG).show();
-            }
-        }
-
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data)
-    {
-        super.onActivityResult(requestCode, resultCode, data);
-        if(resultCode == Activity.RESULT_OK)
-            if (requestCode == GALLERY_REQUEST) {
-                Uri selectedImage = data.getData();
-                try {
-                    bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), selectedImage);
-                    imageView.setImageBitmap(bitmap);
-                } catch (IOException e) {
-                    Log.i("TAG", "Some exception " + e);
-                }
-            }
-        if (requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK)
-        {
-            bitmap = (Bitmap) data.getExtras().get("data");
-            imageView.setImageBitmap(bitmap);
-        }
-    }
 
 
 
--- app/src/main/java/edu/polytech/si3/ihm/plantish/UpdatePlantActivity.java
+++ app/src/main/java/edu/polytech/si3/ihm/plantish/UpdatePlantActivity.java
@@ -1,47 +1,17 @@
 package edu.polytech.si3.ihm.plantish;
 
-import android.Manifest;
-import android.app.Activity;
-import android.app.DatePickerDialog;
-import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.graphics.Bitmap;
-import android.graphics.drawable.Drawable;
-import android.location.Location;
-import android.location.LocationManager;
-import android.net.Uri;
 import android.os.Bundle;
-import android.provider.MediaStore;
-import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.DatePicker;
 import android.widget.EditText;
-import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Spinner;
-import android.widget.Toast;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.preference.PreferenceManager;
 
-import org.osmdroid.api.IMapController;
-import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
-import org.osmdroid.util.GeoPoint;
-import org.osmdroid.views.MapView;
-import org.osmdroid.views.overlay.ItemizedIconOverlay;
-import org.osmdroid.views.overlay.ItemizedOverlayWithFocus;
-import org.osmdroid.views.overlay.OverlayItem;
-
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Calendar;
 import java.util.Date;
-import java.util.List;
 
 import edu.polytech.si3.ihm.plantish.plants.BushFactory;
 import edu.polytech.si3.ihm.plantish.plants.FlowerFactory;
@@ -50,32 +20,14 @@ import edu.polytech.si3.ihm.plantish.plants.PlantFactory;
 import edu.polytech.si3.ihm.plantish.plants.TreeFactory;
 import edu.polytech.si3.ihm.plantish.posts.Post;
 import edu.polytech.si3.ihm.plantish.user.Session;
-import edu.polytech.si3.ihm.plantish.user.User;
 
 /**
  * @author Marine Demonchaux
  */
 
-public class UpdatePlantActivity extends AppCompatActivity {
+public class UpdatePlantActivity extends PlantActivity {
 
-    EditText date;
-    EditText description;
-    Spinner spinnerType;
-    Spinner spinnerFamily;
-    Bitmap bitmap;
-    User user;
-
-    DatePickerDialog datePickerDialog;
-    ImageButton imageButton;
-    private static final int CAMERA_REQUEST = 1888;
-    private ImageView imageView;
-    private static final int MY_CAMERA_PERMISSION_CODE = 100;
-    private static final int MY_PERMISSION_ACCESS_COURSE_LOCATION = 1;
-    private static final int GALLERY_REQUEST = 2;
-    private GeoPoint location;
-
-    private int position;
-    private Post post;
+    private int pos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -85,16 +37,16 @@ public class UpdatePlantActivity extends AppCompatActivity {
         org.osmdroid.config.Configuration.getInstance().load(getApplicationContext(), PreferenceManager.getDefaultSharedPreferences(getApplicationContext()));
 
         this.user = Session.getInstance().getUser();
-        this.position = getIntent().getIntExtra("Position", 0);
+        this.pos = getIntent().getIntExtra("Position", 0);
 
-        this.post = Session.getPost(position);
+        Post post = Session.getPost(pos);
 
         //DATE
         // initiate the date picker and a button
         date = (EditText) findViewById(R.id.date);
         // perform click event on edit text
         setCalendar();
-        date.setText(post.getDate().getDay()+"/"+post.getDate().getMonth()+"/"+post.getDate().getYear());
+        date.setText(post.getDate().getDay()+"/"+ post.getDate().getMonth()+"/"+ post.getDate().getYear());
 
         //** Description
         this.description = (EditText) findViewById(R.id.editText2);
@@ -133,40 +85,23 @@ public class UpdatePlantActivity extends AppCompatActivity {
         //**
 
         //**  Location
-        MyLocationListener.Request_FINE_LOCATION(UpdatePlantActivity.this, MY_PERMISSION_ACCESS_COURSE_LOCATION);
-        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        location = post.getPlant().getPosition();
+        position = post.getPlant().getPosition();
         //**
 
         //** Map
-        if( location != null)
-            initializeMap(location);
+        if( position != null)
+            initializeMap(position);
 
         // Add button
-        Button addButton = (Button) findViewById(R.id.button);
-        addPlant(addButton);
+        Button updateButton = (Button) findViewById(R.id.button);
+        updatePlant(updateButton);
 
     }
 
-    private int getPositionStringInArrayRes(int id, String string){
-        List<String> list = Arrays.asList(getResources().getStringArray(id));
-        string = englishToFrench(string);
-        for(int i = 0; i < list.size(); i++){
-            if(list.get(i).equalsIgnoreCase(string)) return i;
-        }
-        return 0;
-    }
 
-    private String englishToFrench(String s){
-        if(s.equals("FLOWER")) return "Fleur";
-        if(s.equals("BUSH")) return "Buisson";
-        if(s.equals("TREE")) return "Arbre";
-        else return s;
-    }
+    private void updatePlant(Button updateButton){
 
-    private boolean addPlant(Button addButton){
-
-        addButton.setOnClickListener(new View.OnClickListener() {
+        updateButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Date datePost = new Date();
@@ -188,7 +123,7 @@ public class UpdatePlantActivity extends AppCompatActivity {
                 } else {
                     plantFactory = new TreeFactory();
                 }
-                Plant plant = plantFactory.build(location, family, description);
+                Plant plant = plantFactory.build(position, family, description);
                 Post post;
                 if (bitmap == null)
                     post = plantFactory.build(user, datePost, plant);
@@ -196,140 +131,16 @@ public class UpdatePlantActivity extends AppCompatActivity {
                     post = plantFactory.build(user, datePost, bitmap, plant);
 
                 Session.deletePost(post);
-                Session.updatePost(position, post);
+                Session.updatePost(pos, post);
 
                 Intent intent = new Intent(UpdatePlantActivity.this, MyPlantsActivity.class);
                 startActivity(intent);
 
             }
         });
-        return false;
 
     }
 
-    private void setCalendar(){
-        date.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // calender class's instance and get current date , month and year from calender
-                final Calendar c = Calendar.getInstance();
-                int mYear = c.get(Calendar.YEAR); // current year
-                int mMonth = c.get(Calendar.MONTH); // current month
-                int mDay = c.get(Calendar.DAY_OF_MONTH); // current day
-                // date picker dialog
-                datePickerDialog = new DatePickerDialog(UpdatePlantActivity.this,
-                        new DatePickerDialog.OnDateSetListener() {
-
-                            @Override
-                            public void onDateSet(DatePicker view, int year,
-                                                  int monthOfYear, int dayOfMonth) {
-                                // set day of month , month and year value in the edit text
-                                date.setText(String.format("%d/%d/%d", dayOfMonth, monthOfYear + 1, year));
-
-                            }
-                        }, mYear, mMonth, mDay);
-                datePickerDialog.show();
-            }
-        });
-    }
-
-
-    private void galleryButton(){
-        imageButton = (ImageButton) findViewById(R.id.galleryBtn);
-        imageButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
-                photoPickerIntent.setType("image/*");
-                startActivityForResult(photoPickerIntent, GALLERY_REQUEST);
-            }});
-    }
-
-
-
-    private void cameraButton(){
-        imageButton = (ImageButton) findViewById(R.id.cameraBtn);
-        imageButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-                    requestPermissions(new String[]{Manifest.permission.CAMERA}, MY_CAMERA_PERMISSION_CODE);
-                } else {
-                    Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                    startActivityForResult(cameraIntent, CAMERA_REQUEST);
-                }
-            }});
-    }
-
-    private void initializeMap(GeoPoint location){
-        MapView map = findViewById(R.id.mapObject1);
-        map.setTileSource(TileSourceFactory.MAPNIK); //Render
-        map.setBuiltInZoomControls(true); //Zoomable
-        GeoPoint startPoint = new GeoPoint(location.getLatitude(), location.getLongitude()); //Données de Marine ici
-        IMapController controller = map.getController();
-        controller.setZoom(19.2); //Valeur par défault du zoom
-        controller.setCenter(startPoint); //Centrer la carte sur ce point
-
-        List<OverlayItem> marqueurs = new ArrayList<>();
-        OverlayItem home = new OverlayItem("Point A", "Il s'agit du point A", startPoint);
-        Drawable d = home.getMarker(0); //Changer l'image du marqueur, ici un qui existe déjà
-        marqueurs.add(home);
-
-        ItemizedOverlayWithFocus<OverlayItem> itemizedOverlayWithFocus = new ItemizedOverlayWithFocus<>(getApplicationContext(), marqueurs, new ItemizedIconOverlay.OnItemGestureListener<OverlayItem>() {
-            @Override
-            public boolean onItemSingleTapUp(int index, OverlayItem item) {
-                return true; // Pour qu'il réagisse quand j'ai cliqué dessus
-            }
-
-            @Override
-            public boolean onItemLongPress(int index, OverlayItem item) {
-                return false;
-            }
-        });
-        itemizedOverlayWithFocus.setFocusItemsOnTap(true);
-        map.getOverlays().add(itemizedOverlayWithFocus);
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults)
-    {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (requestCode == MY_CAMERA_PERMISSION_CODE)
-        {
-            if (grantResults[0] == PackageManager.PERMISSION_GRANTED)
-            {
-                Toast.makeText(this, "camera permission granted", Toast.LENGTH_LONG).show();
-                Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(cameraIntent, CAMERA_REQUEST);
-            }
-            else
-            {
-                Toast.makeText(this, "camera permission denied", Toast.LENGTH_LONG).show();
-            }
-        }
-
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data)
-    {
-        super.onActivityResult(requestCode, resultCode, data);
-        if(resultCode == Activity.RESULT_OK)
-            if (requestCode == GALLERY_REQUEST) {
-                Uri selectedImage = data.getData();
-                try {
-                    bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), selectedImage);
-                    imageView.setImageBitmap(bitmap);
-                } catch (IOException e) {
-                    Log.i("TAG", "Some exception " + e);
-                }
-            }
-        if (requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK)
-        {
-            bitmap = (Bitmap) data.getExtras().get("data");
-            imageView.setImageBitmap(bitmap);
-        }
-    }
 
 
 
